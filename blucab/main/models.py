f2bb2b66ca4e2393bafbed7deef0100cd3cbf6cd
Florian Völker <github@floyer.de>
2024-04-26 22:00:44 +0200

    Add model to cover movies

--- blucab/main/models.py
+++ blucab/main/models.py
@@ -16,3 +16,21 @@ class Item(models.Model):
 
     def __str__(self):
         return self.text
+    
+class Movie(models.Model):
+    ean = models.CharField(max_length=16)
+    asin = models.CharField(max_length=16)
+    title = models.CharField(max_length=128)
+    title_clean = models.CharField(max_length=128)
+    format = models.CharField(max_length=16)
+    release = models.CharField(max_length=4)
+    runtime = models.CharField(max_length=5)
+    fsk = models.CharField(max_length=50)
+    content = models.CharField(max_length=10000)
+    actor = models.CharField(max_length=500)
+    regisseur = models.CharField(max_length=100)
+    studio = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title_clean
+
