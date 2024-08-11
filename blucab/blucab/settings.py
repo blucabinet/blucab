1210939165eb344bffac447f8d3325c8640ea969
Florian Völker <github@floyer.de>
2024-08-12 00:28:37 +0200

    Env: removed duplicate SECRET_KEY entry in settings

--- blucab/blucab/settings.py
+++ blucab/blucab/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = env(
+SECRET_KEY = env(
     "SECRET_KEY", "django-insecure-3@vfzox&c=0vq#=&ahgc3uyq7sv_y%391)r=3gl(h0(s0b8=t$"
 )
 
