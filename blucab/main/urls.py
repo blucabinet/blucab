7ff8130908c8c28eca723ec92fa39b9a84f16841
Florian Völker <github@floyer.de>
2024-08-12 22:06:49 +0200

    View: Cab added tests to show other users publicly if they wish

--- blucab/main/urls.py
+++ blucab/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:id>", views.index, name="index"),
+    path("cab/<str:uname>", views.index, name="index"),
     path("", views.home, name="home"),
     path("create/", views.create, name="create"),
     path("view/", views.view, name="view"),
