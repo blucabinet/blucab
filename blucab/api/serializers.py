5b7523ceb8ce241c5cfa3472fbc17f7baeccfc70
Florian Völker <github@floyer.de>
2024-08-23 21:35:54 +0200

    API: Add custom field for Movie serializer

--- blucab/api/serializers.py
+++ blucab/api/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from main.models import Movie
+from django.templatetags.static import static
 
 
 class MovieSerializer(serializers.ModelSerializer):
+
+    picture_url = serializers.SerializerMethodField("assembled_url")
+
+    def assembled_url(self, Movie):
+
+        if Movie.picture_available:
+            file_path = "main/cover/"
+            file_name = Movie.ean
+        else:
+            file_path = "main/"
+            file_name = "dummy"
+
+        path = file_path + file_name + ".jpg"
+        url = static(path)
+        return url
+
     class Meta:
         model = Movie
         fields = [
@@ -26,4 +43,5 @@ class MovieSerializer(serializers.ModelSerializer):
             "episode_count",
             "is_series",
             "picture_available",
+            "picture_url",
         ]
