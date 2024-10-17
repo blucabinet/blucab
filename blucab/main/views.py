abd99e52b109efcb3b5b9918c475abbc0a0413cf
Florian Völker <github@floyer.de>
2024-10-17 20:25:39 +0200

    Cleanup: Remove old create references

--- blucab/main/views.py
+++ blucab/main/views.py
@@ -11,9 +11,6 @@ import os
 import requests
 
 # Create your views here.
-url = "https://m.media-amazon.com/images/I/51DUcBqDTcL._SX300_SY300_QL70_ML2_.jpg"
-
-
 def handler_400(request, exception):
     return render(request, "error/400.html", status=400)
 
@@ -54,29 +51,6 @@ def index(response, uname):
     for movie in ls:
         print(movie.movie.title_clean)
 
-    # if ls in response.user.todolist.all():
-    #
-    #    if response.method == "POST":
-    #        print(response.POST)
-    #        if response.POST.get("save"):
-    #            for item in ls.item_set.all():
-    #                if response.POST.get("c" + str(item.id)) == "clicked":
-    #                    item.complete = True
-    #                else:
-    #                    item.complete = False
-    #
-    #                item.save()
-    #
-    #        elif response.POST.get("newItem"):
-    #            txt = response.POST.get("new")
-    #
-    #            # Validity check
-    #            if len(txt) > 2:
-    #                ls.item_set.create(text=txt, complete=False)
-    #            else:
-    #                print("invalid input")
-    #
-    #    return render(response, "main/list.html", {"ls": ls})
     return render(response, "main/view.html", {})
 
 
@@ -96,24 +70,6 @@ def csv_import(response):
 
 def home(response):
     return render(response, "main/home.html", {})
-
-
-def create(response):
-    if response.method == "POST":
-        form = CreateNewList(response.POST)
-
-        if form.is_valid():
-            n = form.cleaned_data["name"]
-            t = ToDoList(name=n)
-            t.save()
-            response.user.todolist.add(t)
-
-        return HttpResponseRedirect("/%i" % t.id)
-
-    else:
-        form = CreateNewList()
-
-    return render(response, "main/create.html", {"form": form})
 
 
 def view(response):
