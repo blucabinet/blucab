b7144f94cef1ac82f3444d3b7010d33deeafb765
Florian Völker <github@floyer.de>
2025-07-20 23:29:45 +0200

    views: Return 403 if user is not authenticated

--- blucab/main/views.py
+++ blucab/main/views.py
@@ -36,6 +36,7 @@ def privacy(request):
 
 def cab_uname(request, uname):
     user_id_query = User.objects.filter(username=uname)
+    
     if len(user_id_query) != 1:
         return render(request, "error/403_user_not_public.html", {})
 
@@ -58,37 +59,37 @@ def cab_uname(request, uname):
 def csv_import(request):
     user = request.user
 
-    if user.is_authenticated:
-        if request.method == "POST":
-            file = request.FILES.get("myfile", None)
+    if not user.is_authenticated:
+        return render(request, "error/403.html", {})
+    
+    if request.method == "POST":
+        file = request.FILES.get("myfile", None)
 
-            if not file:
-                return render(
-                    request,
-                    "main/csv_import.html",
-                    {"message": "Please select a file to upload."},
-                )
-
-            myfile = request.FILES["myfile"]
-            file_path = os.path.join(settings.BASE_DIR, "import")
-
-            filestorage = FileSystemStorage(location=str(file_path))
-            filename = filestorage.save(myfile.name, myfile)
-            uploaded_file_url = filestorage.url(filename)
-
-            ch = handler()
-            ch.csv_importer(filename=filename, user=user)
-            ch.content_update()
-
-            os.remove(os.path.join(settings.BASE_DIR, "import", filename))
-
+        if not file:
             return render(
                 request,
                 "main/csv_import.html",
-                {"uploaded_file_url": uploaded_file_url},
+                {"message": "Please select a file to upload."},
             )
-    else:
-        pass
+
+        myfile = request.FILES["myfile"]
+        file_path = os.path.join(settings.BASE_DIR, "import")
+
+        filestorage = FileSystemStorage(location=str(file_path))
+        filename = filestorage.save(myfile.name, myfile)
+        uploaded_file_url = filestorage.url(filename)
+
+        ch = handler()
+        ch.csv_importer(filename=filename, user=user)
+        ch.content_update()
+
+        os.remove(os.path.join(settings.BASE_DIR, "import", filename))
+
+        return render(
+            request,
+            "main/csv_import.html",
+            {"uploaded_file_url": uploaded_file_url},
+        )
 
     return render(request, "main/csv_import.html", {})
 
@@ -100,25 +101,26 @@ def home(request):
 def add_movie(request):
     user = request.user
 
-    if user.is_authenticated:
-        return render(request, "main/add_movie.html", {})
-    else:
+    if not user.is_authenticated:
         return render(request, "error/403.html", {})
+        
+    return render(request, "main/add_movie.html", {})
+
 
 
 def view(request):
     user = request.user
 
-    if user.is_authenticated:
-        usersettings = user.user_profile
-        movieuserlist = MovieUserList.objects.all().filter(user=user)
-        return render(
-            request,
-            "main/view.html",
-            {"movieuserlist": movieuserlist, "usersettings": usersettings, "is_user_view": True},
-        )
-    else:
+    if not user.is_authenticated:
         return render(request, "error/403.html", {})
+
+    usersettings = user.user_profile
+    movieuserlist = MovieUserList.objects.all().filter(user=user)
+    return render(
+        request,
+        "main/view.html",
+        {"movieuserlist": movieuserlist, "usersettings": usersettings, "is_user_view": True},
+    )
 
 
 def user_settings(request):
