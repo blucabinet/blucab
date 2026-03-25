184de4b73121af5077da86623786351744852494
Florian Völker <github@floyer.de>
2026-03-25 20:47:48 +0100

    language, model: Add verbose names to all models and add language support

--- blucab/main/models.py
+++ blucab/main/models.py
@@ -4,49 +4,52 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class Movie(models.Model):
-    ean = models.CharField(max_length=16)
-    asin = models.CharField(max_length=16)
-    title = models.CharField(max_length=128)
-    title_clean = models.CharField(max_length=128)
-    format = models.CharField(max_length=16)
+    ean = models.CharField(max_length=16, verbose_name=_("EAN"))
+    asin = models.CharField(max_length=16, verbose_name=_("ASIN"))
+    title = models.CharField(max_length=128, verbose_name=_("Title"))
+    title_clean = models.CharField(max_length=128, verbose_name=_("Title Clean"))
+    format = models.CharField(max_length=16, verbose_name=_("Format"))
     release_year = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(9999)],
         blank=True,
         null=True,
         default=None,
+        verbose_name=_("Release Year"),
     )
-    runtime = models.IntegerField(blank=True, null=True)
-    fsk = models.CharField(max_length=50, blank=True, null=True)
+    runtime = models.IntegerField(blank=True, null=True, verbose_name=_("Runtime"))
+    fsk = models.CharField(max_length=50, blank=True, null=True, verbose_name=_("FSK"))
     fsk_nbr = models.IntegerField(
         validators=[MinValueValidator(-1), MaxValueValidator(100)],
         blank=True,
         null=True,
         default=None,
+        verbose_name=_("FSK NBR"),
     )
-    content = models.CharField(max_length=10000, blank=True, null=True)
-    actor = models.CharField(max_length=500, blank=True, null=True)
-    regisseur = models.CharField(max_length=500, blank=True, null=True)
-    studio = models.CharField(max_length=500, blank=True, null=True)
-    genre = models.CharField(max_length=500, blank=True, null=True)
-    language = models.CharField(max_length=500, blank=True, null=True)
-    disc_count = models.IntegerField(default=1)
-    movie_count = models.IntegerField(default=1)
-    season_count = models.IntegerField(default=0)
-    episode_count = models.IntegerField(default=0)
-    is_series = models.BooleanField(default=False)
-    is_bluray_uhd = models.BooleanField(default=False)
-    picture_available = models.BooleanField(default=False)
-    picture_url_original = models.CharField(max_length=256, blank=True, null=True)
-    picture_url_original_hd = models.CharField(max_length=256, blank=True, null=True)
-    picture_processed = models.BooleanField(default=False)
-    needs_parsing = models.BooleanField(default=True)
-    imdb_id = models.CharField(max_length=100, blank=True, null=True)
+    content = models.CharField(max_length=10000, blank=True, null=True, verbose_name=_("Content"))
+    actor = models.CharField(max_length=500, blank=True, null=True, verbose_name=_("Actor"))
+    regisseur = models.CharField(max_length=500, blank=True, null=True, verbose_name=_("Regisseur"))
+    studio = models.CharField(max_length=500, blank=True, null=True, verbose_name=_("Studio"))
+    genre = models.CharField(max_length=500, blank=True, null=True, verbose_name=_("Genre"))
+    language = models.CharField(max_length=500, blank=True, null=True, verbose_name=_("Language"))
+    disc_count = models.IntegerField(default=1, verbose_name=_("Disc Count"))
+    movie_count = models.IntegerField(default=1, verbose_name=_("Movie Count"))
+    season_count = models.IntegerField(default=0, verbose_name=_("Season Count"))
+    episode_count = models.IntegerField(default=0, verbose_name=_("Episode Count"))
+    is_series = models.BooleanField(default=False, verbose_name=_("Is Series"))
+    is_bluray_uhd = models.BooleanField(default=False, verbose_name=_("Is Blu-ray UHD"))
+    picture_available = models.BooleanField(default=False, verbose_name=_("Picture Available"))
+    picture_url_original = models.CharField(max_length=256, blank=True, null=True, verbose_name=_("Picture URL Original"))
+    picture_url_original_hd = models.CharField(max_length=256, blank=True, null=True, verbose_name=_("Picture URL Original HD"))
+    picture_processed = models.BooleanField(default=False, verbose_name=_("Picture Processed"))
+    needs_parsing = models.BooleanField(default=True, verbose_name=_("Needs Parsing"))
+    imdb_id = models.CharField(max_length=100, blank=True, null=True, verbose_name=_("IMDb ID"))
 
     class Meta:
-        verbose_name = "Movies"
+        verbose_name = _("Movies")
         verbose_name_plural = verbose_name
 
     def __str__(self):
@@ -55,26 +58,26 @@ class Movie(models.Model):
 
 class MovieUserList(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="movieuserlist", null=True
+        User, on_delete=models.CASCADE, related_name="movieuserlist", null=True, verbose_name=_("User")
     )
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True)
-    activated = models.BooleanField(default=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, null=True, verbose_name=_("Movie"))
+    activated = models.BooleanField(default=True, verbose_name=_("Activated"))
     rating = models.IntegerField(
-        default=0, validators=[MinValueValidator(0), MaxValueValidator(6)]
+        default=0, validators=[MinValueValidator(0), MaxValueValidator(6)], verbose_name=_("Rating"),
     )
-    viewed = models.BooleanField(default=False)
-    rented = models.BooleanField(default=False)
-    rented_to = models.CharField(max_length=100, blank=True)
-    date_added = models.DateField(default=timezone.now)
-    price = models.DecimalField(default=0, max_digits=6, decimal_places=2)
-    archived = models.BooleanField(default=False)
+    viewed = models.BooleanField(default=False, verbose_name=_("Viewed"))
+    rented = models.BooleanField(default=False, verbose_name=_("Rented"))
+    rented_to = models.CharField(max_length=100, blank=True, verbose_name=_("Rented to"))
+    date_added = models.DateField(default=timezone.now, verbose_name=_("Date added"))
+    price = models.DecimalField(default=0, max_digits=6, decimal_places=2, verbose_name=_("Price"))
+    archived = models.BooleanField(default=False, verbose_name=_("Archived"))
 
     class Meta:
         unique_together = (
             "user",
             "movie",
         )
-        verbose_name = "User Movie List"
+        verbose_name = _("User Movie List")
         verbose_name_plural = verbose_name
 
     def __str__(self):
@@ -86,47 +89,49 @@ class MovieUserList(models.Model):
 
 class UserSettings(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name="user_profile"
+        User, on_delete=models.CASCADE, related_name="user_profile", verbose_name=_("User")
     )
     price_unit = models.CharField(
-        default="€", max_length=3, blank=True, verbose_name="Price Unit"
+        default="€", max_length=3, blank=True, verbose_name=_("Price Unit")
     )
     days_for_new = models.IntegerField(
         default=30,
         validators=[MinValueValidator(0), MaxValueValidator(730)],
-        verbose_name="Days since adding a movie to show the new sign",
+        verbose_name=_("Days since adding a movie to show the new sign"),
     )
     view_is_public = models.BooleanField(
-        default=False, verbose_name="The library can be viewed publicly"
+        default=False, verbose_name=_("The library can be viewed publicly")
     )
-    show_view_title = models.BooleanField(default=True, verbose_name="Show Movie title")
+    show_view_title = models.BooleanField(
+        default=True, verbose_name=_("Show Movie title")
+    )
     show_view_details = models.BooleanField(
-        default=True, verbose_name="Show Movie details"
+        default=True, verbose_name=_("Show Movie details")
     )
     show_view_icon_new = models.BooleanField(
-        default=True, verbose_name="Show the new sign"
+        default=True, verbose_name=_("Show the new sign")
     )
     show_view_icon_rented = models.BooleanField(
-        default=True, verbose_name="Show the rented sign"
+        default=True, verbose_name=_("Show the rented sign")
     )
     show_view_count_disc = models.BooleanField(
-        default=True, verbose_name="- Show the number of disks"
+        default=True, verbose_name=_("Show the number of disks")
     )
     show_view_count_movie = models.BooleanField(
-        default=True, verbose_name="- Show the number of movies"
+        default=True, verbose_name=_("Show the number of movies")
     )
     show_view_button_details = models.BooleanField(
-        default=True, verbose_name="Show the details button"
+        default=True, verbose_name=_("Show the details button")
     )
     show_view_button_share = models.BooleanField(
-        default=True, verbose_name="Show the share button"
+        default=True, verbose_name=_("Show the share button")
     )
     last_export = models.DateTimeField(
-        blank=True, null=True, verbose_name="Last time an export was executed"
+        blank=True, null=True, verbose_name=_("Last time an export was executed")
     )
 
     class Meta:
-        verbose_name = "User Settings"
+        verbose_name = _("User Settings")
         verbose_name_plural = verbose_name
 
     def __str__(self):
