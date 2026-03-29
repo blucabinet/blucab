84721355fbd97a05d56f24958480b5734635e334
Florian Völker <github@floyer.de>
2026-03-29 20:23:26 +0200

    register: Email needs to be entered twice as confirmation

--- blucab/register/forms.py
+++ blucab/register/forms.py
@@ -1,14 +1,26 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(label=(_("Email")), required=True)
+    email_confirm = forms.EmailField(label=(_("Email confirmation")), required=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "email_confirm", "password1", "password2"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get("email")
+        email_confirm = cleaned_data.get("email_confirm")
+
+        if email and email_confirm and email != email_confirm:
+            self.add_error('email_confirm', _("The Email confirmation does not match the Email."))
+        
+        return cleaned_data
 
 
 class ChangePasswordForm(PasswordChangeForm):
