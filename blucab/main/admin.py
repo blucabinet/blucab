bfaf1b3db7adf59096c62a13e3b72890bdee476d
Florian Völker <github@floyer.de>
2026-03-29 22:36:39 +0200

    admin: Rebrand the admin interface

--- blucab/main/admin.py
+++ blucab/main/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Movie, MovieUserList, UserSettings
+from django.utils.translation import gettext as _
 
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(MovieUserList)
 admin.site.register(UserSettings)
+
+admin.site.site_title = _("blucab site admin")
+admin.site.site_header = _("blucab administration")
