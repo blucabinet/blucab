0f5d86b2d929ddf2f2e1c9fe5be3f6228bfe2127
Florian Völker <github@floyer.de>
2026-03-30 22:58:45 +0200

    register: Rename register url with common prefix "user"

--- blucab/register/urls.py
+++ blucab/register/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
-    path('user/activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path("user/register/", views.register, name="register"),
+    path("user/activate/<uidb64>/<token>/", views.activate, name="activate"),
     path("user/change_password/", views.change_password, name="change_password"),
     path(
         "user/change_password/done/",
