b50733f6fc043ce4d1a30035e17a425e61c0d1b7
Florian Völker <github@floyer.de>
2026-04-16 22:45:03 +0200

    admin: Add new UserCabinet model to Admin view

--- blucab/main/admin.py
+++ blucab/main/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Movie, MovieUserList, UserSettings
+from .models import Movie, MovieUserList, UserSettings, UserCabinet
 from django.utils.translation import gettext as _
 
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(MovieUserList)
 admin.site.register(UserSettings)
+admin.site.register(UserCabinet)
 
 admin.site.site_title = _("blucab site admin")
 admin.site.site_header = _("blucab administration")
