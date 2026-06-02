60aada055d48fb63f4272aaa37c95a33d8e87c84
Florian Völker <github@floyer.de>
2026-06-02 23:36:20 +0200

    model: New contenthandler fields for differentiation between new or updated movie

--- blucab/contenthandler/models.py
+++ blucab/contenthandler/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from main.models import Movie
 
 
 class FailedAddMovie(models.Model):
@@ -8,7 +9,13 @@ class FailedAddMovie(models.Model):
     date_added = models.DateTimeField(
         default=timezone.now, verbose_name=_("Date added")
     )
+    is_movie_update = models.BooleanField(
+        default=False, verbose_name=_("Is Existing Movie")
+    )
     checked = models.BooleanField(default=False, verbose_name=_("Checked"))
+    movie = models.ForeignKey(
+        Movie, null=True, blank=True, on_delete=models.SET_NULL, verbose_name=_("Movie")
+    )
 
     class Meta:
         verbose_name = _("Failed Movie Import")
